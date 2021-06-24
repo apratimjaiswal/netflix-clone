fbd815779704fc7db5af8a829ef5ac8984a7daa8
Apratim <apratim99@gmail.com>
2021-06-25 04:20:36 +0530

    Commit - netflix logo & avatar local files added

--- src/Nav.js
+++ src/Nav.js
@@ -26,14 +26,16 @@ function Nav() {
         <img
           onClick={() => history.push("/")}
           className="nav__logo"
-          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          // src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          src="../netflix-logo.png"
           alt=""
         />
 
         <img
           onClick={() => history.push("/profile")}
           className="nav__avatar"
-          src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+          // src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+          src="../Netflix-avatar.png"
           alt=""
         />
       </div>
